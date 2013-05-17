b7b32c26e76afa668f1d078906b4865fb758949e
James Tunnicliffe <james@nanosheep.org>
2013-05-17 20:35:47 +0100

    Added some license details.

--- local_settings_template.py
+++ local_settings_template.py
@@ -1,3 +1,20 @@
+# Copyright 2013: James Tunnicliffe
+#
+# This file is part of Need Input.
+#
+# Need Input is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Need Input is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Need Input.  If not, see <http://www.gnu.org/licenses/>.
+
 JIRA_LOGIN = ("<username>", "<password>")
 LP_USER = "<username>"
 GMAIL_USER = "<email address>"
