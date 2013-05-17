f621aba4f443b4cea0fc4923c4d9ac6532523b4d
James Tunnicliffe <james@nanosheep.org>
2013-05-17 20:22:07 +0100

    Added template for local_settings.py

---
+++ local_settings_template.py
@@ -0,0 +1,12 @@
+JIRA_LOGIN = ("<username>", "<password>")
+LP_USER = "<username>"
+GMAIL_USER = "<email address>"
+GMAIL_PASSWORD = "<password>"
+
+IGNORE = {
+    "lp_bp": [
+        "<url of a blueprint you want to ignore>"
+    ]
+}
+
+CAL_EMBED = """<iframe embed code from google calendar -- see https://support.google.com/calendar/answer/41207?hl=en>"""
